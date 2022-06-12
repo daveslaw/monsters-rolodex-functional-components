67b2ee95618433111fce8673dd32e7114b46f877
Dave <daveslaw@gmail.com>
2022-06-12 13:57:00 +0300

    completion and formatted

--- src/components/SearchBox/SearchBox.js
+++ src/components/SearchBox/SearchBox.js
@@ -1,10 +1,15 @@
 import React from "react";
-import './search-box.styles.css';
+import "./search-box.styles.css";
 
 export default function SearchBox({ handleSearch, placeholder, className }) {
 	return (
 		<div>
-			<input className={`search-box ${className}`} type="search" onChange={handleSearch} placeholder={placeholder} />
+			<input
+				className={`search-box ${className}`}
+				type="search"
+				onChange={handleSearch}
+				placeholder={placeholder}
+			/>
 		</div>
 	);
 }
